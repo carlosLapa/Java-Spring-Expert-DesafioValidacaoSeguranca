1f3aabec8133bfcbdd586ea7e0849f491ce2152a
carlosLapa <carlosferreiralapa@gmail.com>
2023-10-13 16:26:43 +0100

    Repositories, Controllers and Services templates created

---
+++ src/main/java/com/devsuperior/demo/controllers/CityController.java
@@ -0,0 +1,12 @@
+package com.devsuperior.demo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/cities")
+public class CityController {
+
+
+
+}
